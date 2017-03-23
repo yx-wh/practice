ae2565e9e95cdaaff0e185ee227a57447e718ca9
yx-wh <yx_wh@hotmail.com>
2017-03-24 02:22:49 +0800

    update strrok_r

--- c/strtok.c
+++ c/strtok.c
@@ -25,13 +25,38 @@ char *mystrtok(char *str, const char *delim)
 		}
 	return start;
 }
+char *mystrtok_r(char *str, const char *delim, char **saveptr)
+{
+	char *start;
+	if (str != NULL)
+	{
+		start = str;
+	}
+	else
+	{
+		start = *saveptr;
+	}
+	for (char *i = start; *i != '\0'; i++)
+		for (const char *j = delim; *j != '\0'; j++)
+		{
+			if (*i == *j)
+			{
+				*i = '\0';
+				*saveptr = 1 + i;
+				return start;
+			}
+		}
+	return start;
+
+}
 
 int main (int argc, char *argv[])
 {
 	char str1[] = "a/bbb///cc;xxx:yyy:";
 	char str2[] = "a/bbb///cc;xxx:yyy:";
+	char str3[] = "a/bbb///cc;xxx:yyy:";
 	char *stra = ";:";
-	//char *saveptr1;
+	char *saveptr1;
 	char *token;
 	//token = strtok_r(str1, str2, &saveptr1);
 	//printf("%s\n", token);
@@ -51,6 +76,13 @@ int main (int argc, char *argv[])
 	token = strtok(NULL, stra);
 	printf("%s\n", token);
 	token = strtok(NULL, stra);
+	printf("%s\n", token);
+
+	token = mystrtok_r(str3, stra, &saveptr1);
+	printf("%s\n", token);
+	token = mystrtok_r(NULL, stra, &saveptr1);
+	printf("%s\n", token);
+	token = mystrtok_r(NULL, stra, &saveptr1);
 	printf("%s\n", token);
 	return 0;
 }
