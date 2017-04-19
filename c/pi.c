beece759473bfcb58cb9019de32a3c2824aae492
yx-wh <yx_wh@hotmail.com>
2017-04-19 12:53:00 +0800

    change pi.c

--- c/pi.c
+++ c/pi.c
@@ -3,16 +3,37 @@
 #define N 10000
 #define C 20
 #define R 19
+uint64_t a = 1000000, b = 0, c = N * R, d, e, f[N * R + 1], g;
 
 int main(void)
 {
-	uint64_t a = 1000000, b = 0, c = N * R, d, e, f[N * R + 1], g;
 
 
 	for (; b - c;)
 		f[b++] = C * N;
-	for (; d = 0, g = c * 2; c -= R, printf("%.6d", e + d / a), e = d % a)
-		for (b = c; d += f[b] * a, f[b] = d % --g, d /= g--, --b;
-		     d *= b) ;
+	//for (; d = 0, g = c * 2; c -= R, printf("%.6d", e + d / a), e = d % a)
+	//	for (b = c; d += f[b] * a, f[b] = d % --g, d /= g--, --b;
+	//	     d *= b) ;
+	while (c != 0)
+	{
+		d = 0;
+		g = c * 2;
+		b = c;
+		while (1)
+		{
+			d = d + f[b] * a;
+			g--;
+			f[b] = d % g;
+			d = d / g;
+			g--;
+			b--;
+			if (b == 0)
+				break;
+			d = d * b;
+		}
+		c = c - R;
+		printf("%.6d", e + d / a);
+		e = d % a;
+	}
 	return 0;
 }
