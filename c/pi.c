966f8d385f33c8cf775f0c08a1d9d5e38482eb0e
yx-wh <yx_wh@hotmail.com>
2017-04-19 18:27:09 +0800

    change pi.c

--- c/pi.c
+++ c/pi.c
@@ -9,8 +9,9 @@ int main(void)
 	uint64_t a = 1000000, b = 0, c = N * R, d, e = 0, f[N * R + 1], g;
 	//printf("%d\n", e);
 
-	for (; b - c;)
-		f[b++] = C * N;
+	f[0] = C * N;
+	while (b != c)
+		f[++b] = C * N;
 	//for (; d = 0, g = c * 2; c -= R, printf("%.6d", e + d / a), e = d % a)
 	//	for (b = c; d += f[b] * a, f[b] = d % --g, d /= g--, --b;
 	//	     d *= b) ;
