cddb3b38d45e83f77ce6ca6ca0504fb0763eac41
yx <yx_wh@hotmail.com>
2018-03-31 16:58:29 +0800

    update 1007. Maximum Subsequence Sum (25)

--- c/pat-a-1007.c
+++ c/pat-a-1007.c
@@ -3,60 +3,107 @@
 //10
 //-10 1 2 3 4 -5 -23 3 7 -21
 //出来的结果就有问题
-#include <stdio.h>
 
-int main(void)
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
 {
-	int count;
-	scanf("%d", &count);
-	int a[count];
-	int b[count];
-	int c[count];
-	int i,j,sum;
-	for (int k = 0; k < count; k++)
-	{
-		scanf("%d", &a[k]);
-	}
-	for (int k = 0; k <= count; k++)
-	{
-		if (k == count)
-		{
-			//printf("%d %d %d", 0, a[0], a[count - 1]);
-			printf("0 0 0");
-			return 0;
+	int k;
+	int *data;
+	int i;
+
+	int start, end, sum, temp, tempi, tempj;
+
+	while (scanf("%d", &k) != EOF) {
+		data = (int *)malloc(k * sizeof(int));
+		for (i = 0; i < k; i++) {
+			scanf("%d", &data[i]);
 		}
-		if (a[k] > 0)
-			break;
+		sum = 0;
+		start = 0;
+		end = k - 1;
+		temp = 0;
+		tempi = 0;
+		tempj = 0;
+		for (i = 0; i < k; i++) {
+			if (temp >= 0) {
+				temp += data[i];
+				tempj = i;
+			} else {
+				temp = data[i];
+				tempi = i;
+				tempj = i;
+			}
+
+			/*为什么这里是temp大于等于0就继续加，而小于0就新开一块呢，
+			 *可以假设有这样的情况，A、B都是一串数字，x是个负数。
+			 *  [A]x[B]
+			 *设A=m，若A+x<0，那么x<-m。要想使A+x+B大于m，那么必然B>m而且B的绝对值要比x大。
+			 *Maximum Subsequence Sum就该是B了。所以这个时候要重新算temp。
+			 *而且注意和相等的时候i和j的值要最小。
+			 *10
+			 *-10 1 2 3 4 -5 -23 3 7 -21
+			 *(gdb) i locals 
+			 *k = 10
+			 *data = 0x555555756420
+			 *i = 4
+			 *start = 1
+			 *end = 4
+			 *sum = 10
+			 *temp = 10
+			 *tempi = 1
+			 *tempj = 4
+			 *(gdb) i locals 
+			 *k = 10
+			 *data = 0x555555756420
+			 *i = 5
+			 *start = 1
+			 *end = 4
+			 *sum = 10
+			 *temp = 5
+			 *tempi = 1
+			 *tempj = 5
+			 *(gdb) i locals 
+			 *k = 10
+			 *data = 0x555555756420
+			 *i = 6
+			 *start = 1
+			 *end = 4
+			 *sum = 10
+			 *temp = -18
+			 *tempi = 1
+			 *tempj = 6
+			 *(gdb) i locals 
+			 *k = 10
+			 *data = 0x555555756420
+			 *i = 9
+			 *start = 1
+			 *end = 4
+			 *sum = 10
+			 *temp = -11
+			 *tempi = 7
+			 *tempj = 9
+			 *(gdb) i locals 
+			 *k = 10
+			 *data = 0x555555756420
+			 *i = 10
+			 *start = 1
+			 *end = 4
+			 *sum = 10
+			 *temp = -11
+			 *tempi = 7
+			 *tempj = 9
+			 *
+			 */
+			if (temp > sum || (temp == 0 && end == k - 1))	//temp确实比sum大, 或者, sum依然是初始状态(end == k-1即表示)而且temp==0, 需要替换
+			{
+				sum = temp;
+				start = tempi;
+				end = tempj;
+			}
+		}
+		printf("%d %d %d\n", sum, data[start], data[end]);
 	}
-	b[0] = a[0];
-	for (int k = 1; k < count; k++)
-	{
-		b[k] = a[k] + b[k - 1];
-	}
-	j = 0;
-	for (int k = 1; k < count; k++)
-	{
-		if (b[k] > b[j])
-			j = k;
-	}	
-	c[count - 1] = a[count - 1];
-	for (int k = count - 2; k >= 0; k--)
-	{
-		c[k] = a[k] + c[k + 1];
-		//printf("%d ", c[k]);
-	}
-	i = count - 1;
-	//printf("%d ", i);
-	for (int k = count - 2; k >= 0; k--)
-	{
-		if (c[k] > c[i])
-			i = k;
-	}
-	sum = 0;
-	for (int k = i; k <= j; k++)
-	{
-		sum += a[k];
-	}
-	printf("%d %d %d", sum, a[i], a[j]);
 	return 0;
 }
